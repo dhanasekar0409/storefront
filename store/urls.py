70ad987116b6ef0c399b75d3efb8c6a918693f62
Dhanasekar <dhanasekarn0409@gmail.com>
2024-07-18 23:01:01 +0530

    implement nested routes

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,16 @@
+from cgitb import lookup
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
+# Parent Router
 router = DefaultRouter()
 router.register("products", views.ProductViewSet)
 router.register("collections", views.Collection1ViewSet)
-urlpatterns = router.urls
+
+# Child Router
+products_router = routers.NestedDefaultRouter(router, "products", lookup="product")
+products_router.register("reviews", views.ReviewViewSet, basename="product-review")
+
+# URLCONFIG
+urlpatterns = router.urls + products_router.urls
