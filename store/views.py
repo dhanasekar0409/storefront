191966fa22a05c59f806442c901c35115aff054b
Dhanasekar <dhanasekarn0409@gmail.com>
2024-07-18 19:46:47 +0530

    class based views

--- store/views.py
+++ store/views.py
@@ -2,42 +2,43 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .models import Collection1, Product
 from .serializers import Collection1Serializer, ProductSerializer
 from django.db.models import Count
 
 
-# Create your views here.
-@api_view(["GET", "POST"])
-def product_list(request):
-    if request.method == "GET":
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.select_related("collection").all()
         serializer = ProductSerializer(
             queryset, many=True, context={"request": request}
         )
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == "GET":
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+class ProductDetail(APIView):
+    def get(self, request,id):
+        product = get_object_or_404(Product, pk=id)
+        if request.method == "GET":
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
 
-    elif request.method == "PUT":
+    def put(self, request,id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    elif request.method == "DELETE":
+    def delete(self, request,id):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:  # type: ignore
             return Response(
                 {
@@ -71,7 +72,7 @@ def collection_detail(request, pk):
     if request.method == "GET":
         serializer = Collection1Serializer(collection)
         return Response(serializer.data)
-    elif request.method == "PUT":
+    elif request.method == "POST":
         serializer = Collection1Serializer(collection, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
