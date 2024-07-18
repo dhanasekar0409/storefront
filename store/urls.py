2c82eda0d50eae85f295a7f8488f422f0fb93208
Dhanasekar <dhanasekarn0409@gmail.com>
2024-07-18 22:32:41 +0530

    implement routing

--- store/urls.py
+++ store/urls.py
@@ -1,14 +1,7 @@
-from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
-# URLConf
-urlpatterns = [
-    path("products/", views.ProductList.as_view()),
-    path("products/<int:pk>/", views.ProductDetail.as_view()),
-    path("collections/", views.Collection1List.as_view()),
-    path(
-        "collections/<int:pk>/",
-        views.CollectionDetail.as_view(),
-        name="collection-detail",
-    ),
-]
+router = DefaultRouter()
+router.register("products", views.ProductViewSet)
+router.register("collections", views.Collection1ViewSet)
+urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -1,8 +1,9 @@
+from itertools import product
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from .models import Collection1, Product
+from .models import Collection1, OrderItem, Product
 from .serializers import Collection1Serializer, ProductSerializer
 from django.db.models import Count
 
@@ -14,24 +15,22 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {"request": self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:  # type: ignore
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:  # type: ignore
             return Response(
                 {
                     "error": "Product can not be deleted because it is associated with an order item"
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
-class CollectionViewSet(ModelViewSet):
+class Collection1ViewSet(ModelViewSet):
     queryset = Collection1.objects.annotate(products_count=Count("products")).all()
     serializer_class = Collection1Serializer
 
-    def delete(self, request, pk):
+    def destroy(self, request, *args, **kwargs):
         collection = Collection1.objects.annotate(products_count=Count("products"))
         if collection.products.count() > 0:  # type: ignore
             return Response(
@@ -40,5 +39,5 @@ class CollectionViewSet(ModelViewSet):
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-            collection.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        collection.delete()
+        return super().destroy(request, *args, **kwargs)
