42115fff8bc2ebcc290de006cf4d8b951b9186f1
Dhanasekar <dhanasekarn0409@gmail.com>
2024-07-18 23:29:04 +0530

    implement sorting

--- store/views.py
+++ store/views.py
@@ -1,21 +1,22 @@
+from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .filters import ProductFilter
 from .models import Collection1, OrderItem, Product, Review
 from .serializers import Collection1Serializer, ProductSerializer, ReviewSerializer
-from django.db.models import Count
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["collection_id"]
     filterset_class = ProductFilter
     search_fields = ["title", "description"]
+    ordering_fields = ["unit_price", "last_update"]
 
     def get_serializer_context(self):
         return {"request": self.request}
