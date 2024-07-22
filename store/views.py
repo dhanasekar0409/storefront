5cea019d05944f04e4a4932def53c91482895654
Dhanasekar <dhanasekarn0409@gmail.com>
2024-07-22 21:41:26 +0530

    Updating a cart item

--- store/views.py
+++ store/views.py
@@ -20,6 +20,7 @@ from .serializers import (
     Collection1Serializer,
     ProductSerializer,
     ReviewSerializer,
+    UpdateCartItemSerializer,
 )
 
 
@@ -82,6 +83,7 @@ class CartViewSet(
 
 class CartItemViewSet(ModelViewSet):
     serializer_class = CartItemSerializer
+    http_method_names = ["get", "post", "patch", "delete"]
 
     def get_serializer_context(self):
         return {
@@ -91,6 +93,8 @@ class CartItemViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.request.method == "POST":
             return AddCartItemSerializer
+        elif self.request.method == "PATCH":
+            return UpdateCartItemSerializer
         return CartItemSerializer
 
     def get_queryset(self):
