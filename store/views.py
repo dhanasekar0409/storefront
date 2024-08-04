ab8254b1ead5091dc1f3b683b0bcd72403cb4765
Dhanasekar <dhanasekarn0409@gmail.com>
2024-08-04 13:10:27 +0530

    Applying permissions

--- store/views.py
+++ store/views.py
@@ -147,5 +147,12 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:  # type: ignore
+            return Order.objects.all()
+        (customer_id, created) = Customer.objects.only("id").get_or_create(user_id=user.id)  # type: ignore
+        return Order.objects.filter(customer_id=customer_id)
